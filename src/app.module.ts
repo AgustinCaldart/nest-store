ab5e3fb1acdbd19a0f4bda103f5c988b160501db
acaldart <caldart123@gmail.com>
2021-12-14 00:16:47 -0300

    quitando ejemplos

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,25 +10,8 @@ const API_KEY = '12345654';
 const API_KEY_PROD = 'prod12123';
 
 @Module({
-  imports: [UsersModule, ProductsModule, HttpModule],
+  imports: [UsersModule, ProductsModule],
   controllers: [AppController],
-  providers: [
-    AppService,
-    {
-      provide: 'API_KEY',
-      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
-    },
-    {
-      provide: 'TASKS',
-      useFactory: async (http: HttpService) => {
-        const tasks = await http.get(
-          'https://jsonplaceholder.typicode.com/todos',
-        );
-        const data = await firstValueFrom(tasks);
-        return data.data;
-      },
-      inject: [HttpService],
-    },
-  ],
+  providers: [AppService],
 })
 export class AppModule {}
