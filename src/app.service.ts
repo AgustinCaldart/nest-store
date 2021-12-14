6cf7cffb60d2562265ea8b0f2febeeb9bef9a9a8
acaldart <caldart123@gmail.com>
2021-12-14 03:03:02 -0300

    tipado de configuracion

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,15 @@
-import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { Inject, Injectable } from '@nestjs/common';
+import { ConfigService, ConfigType } from '@nestjs/config';
+import config from './config';
 
 @Injectable()
 export class AppService {
-  constructor(private config: ConfigService) {}
+  constructor(
+    @Inject(config.KEY) private configService: ConfigType<typeof config>,
+  ) {}
   getHello(): string {
-    const apiKey = this.config.get<string>('API_KEY');
-    const name = this.config.get<string>('DATABASE_NAME');
+    const apiKey = this.configService.apiKey;
+    const name = this.configService.database.name;
     return apiKey + name;
   }
 }
