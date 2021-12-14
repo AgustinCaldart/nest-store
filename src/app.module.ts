a1776297934bc698d02f57151a8177faaac38756
acaldart <caldart123@gmail.com>
2021-12-14 00:17:01 -0300

    quitando ejemplos

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
-import { HttpModule, HttpService } from '@nestjs/axios';
-import { firstValueFrom } from 'rxjs';
-
-const API_KEY = '12345654';
-const API_KEY_PROD = 'prod12123';
 
 @Module({
   imports: [UsersModule, ProductsModule],
