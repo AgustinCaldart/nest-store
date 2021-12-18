b9484ec211bf4b6f10c098fb5d9c96d78a6e4d5f
acaldart <caldart123@gmail.com>
2021-12-18 15:44:17 -0300

    embebido skill de customer

--- src/users/dtos/customers.dto.ts
+++ src/users/dtos/customers.dto.ts
@@ -1,9 +1,14 @@
 import { PartialType } from '@nestjs/swagger';
-import { IsString, IsNotEmpty } from 'class-validator';
+import { IsString, IsNotEmpty, IsArray } from 'class-validator';
 
 export class CreateCustomerDto {
   @IsString()
   @IsNotEmpty()
   readonly name: string;
+
+  /* Relacionando uno a muuchos embebido
+  @IsNotEmpty()
+  @IsArray()
+  readonly skills: any; */
 }
 export class UpdateCustomerDto extends PartialType(CreateCustomerDto) {}
--- src/users/entities/customer.entity.ts
+++ src/users/entities/customer.entity.ts
@@ -1,9 +1,15 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Document, Types } from 'mongoose';
 
 @Schema()
 export class Customer extends Document {
   @Prop({ required: true, index: true })
   name: string;
+
+  /*Relacion uno a mucho embebido
+ @Prop({
+    type: [{ name: { type: String }, color: { type: String } }],
+  })
+  skills: Types.Array<Record<string, any>>; */
 }
 export const CustomerSchema = SchemaFactory.createForClass(Customer);
