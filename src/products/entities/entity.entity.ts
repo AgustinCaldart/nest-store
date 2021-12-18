d90ee6af8409667710321ad8a82908dd85d32642
acaldart <caldart123@gmail.com>
2021-12-18 06:42:26 -0300

    Schema product

--- src/products/entities/entity.entity.ts
+++ src/products/entities/entity.entity.ts
@@ -1,9 +1,16 @@
-export class Product {
-  id: number;
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+@Schema()
+export class Product extends Document {
+  @Prop({ required: true })
   name: string;
+  @Prop({ required: true })
   description: string;
+  @Prop({ type: Number, required: true })
   price: number;
+  @Prop({ type: Number })
   stock?: number;
+  @Prop()
   image?: string;
 }
 export class Category {
@@ -14,3 +21,5 @@ export class Brand {
   id: number;
   name: string;
 }
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { BrandsController } from './controllers/brands.controller';
 import { CategoriesController } from './controllers/categories.controller';
 import { ProductsController } from './controllers/products.controller';
+import { Product, ProductSchema } from './entities/entity.entity';
 import { BrandsService } from './services/brands.service';
 import { CategoriesService } from './services/categories.service';
 import { ProductsService } from './services/products.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Product.name,
+        schema: ProductSchema,
+      },
+    ]),
+  ],
   controllers: [ProductsController, CategoriesController, BrandsController],
   providers: [ProductsService, CategoriesService, BrandsService],
   exports: [ProductsService],
